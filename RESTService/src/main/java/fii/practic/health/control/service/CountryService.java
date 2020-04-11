925b733d4a8ede4bb2f23910ea5cd2b61945372b
Marius-George Olaru <mariusol@Mac2076s-MacBook-Pro.local>
2020-04-11 22:57:25 +0300

    Added client for SOAP Service

---
+++ RESTService/src/main/java/fii/practic/health/control/service/CountryService.java
@@ -0,0 +1,9 @@
+package fii.practic.health.control.service;
+
+import com.example.wsdl.GetCountryResponse;
+
+public interface CountryService {
+
+    GetCountryResponse getCountry(String country);
+
+}
